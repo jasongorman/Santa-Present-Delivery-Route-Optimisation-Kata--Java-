e4c699e62cdcab0f6f50e38abf322d98c247a6fc
Jason Gorman <jason.gorman@codemanship.com>
2018-12-20 09:39:44 +0000

    Initial commit

---
+++ SantaRouter/test/DistanceTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DistanceTest {
+
+	@Test
+	public void locationIsZeroKmFromItself() {
+		Location location = new Location(0.0, 0.0);
+		assertEquals(0, new City(location, 0).distance(new City(location, 0)), 0.0);
+	}
+	
+	@Test
+	public void newYorkToLosAngeles() {
+		City newYork = new City(new Location(40.6635, -73.9387), 0);
+		City losAngeles = new City(new Location(34.0194, -118.4108), 0);
+		assertEquals(3957, newYork.distance(losAngeles), 1.0); // within 1km
+	}
+	
+	@Test
+	public void houstonToPhoenix() {
+		City houston = new City(new Location(41.8376, -87.6818), 0);
+		City phoenix = new City(new Location(33.5722, -112.0901), 0);
+		assertEquals(2325, houston.distance(phoenix), 1.0);
+	}
+	
+	@Test
+	public void sanDiegoToChicago() {
+		City sanDiego = new City(new Location(32.8153, -117.1350), 0);
+		City chicago = new City(new Location(41.8376, -87.6818), 0);
+		assertEquals(2774, sanDiego.distance(chicago), 1.0);
+	}
+
+}
---
+++ SantaRouter/test/PresentDeliveryTimeTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PresentDeliveryTimeTest {
+
+	@Test
+	public void takesNoTimeToDeliverNoPresents() {
+		assertEquals(0, new Santa(0.001).deliveryTimeHours(0 ), 0.0);
+	}
+	
+	@Test
+	public void takesOneSecondToDeliverOneThousandPresents() {
+		assertEquals((double)1/3600, new Santa(0.001).deliveryTimeHours(1000), 0.0);
+	}
+
+}
